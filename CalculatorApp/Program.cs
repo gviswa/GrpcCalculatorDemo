72385c7bd3cb6e94e0ef4696cb67109b837c6307
Viswanathan Gopalan <gviswa@outlook.com>
2023-07-25 00:09:07 -0400

    GRPC Calculator Demo Implementation

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using CalculatorServer;
+using Grpc.Net.Client;
+
+namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:7025/");
+            var client = new Calculator.CalculatorClient(channel);
+            var calculatorResponse = client.Add(new CalculatorRequest()
+            {
+                Num1 = 1,
+                Num2 = 2,
+            });
+            Console.WriteLine($"The sum is : {calculatorResponse.Result}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CalculatorServer/Services/CalculateService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+
+namespace CalculatorServer.Services
+{
+    public class CalculateService : Calculator.CalculatorBase
+    {
+        public override Task<CalculatorResponse> Add(CalculatorRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CalculatorResponse()
+            {
+                Result = request.Num1 + request.Num2
+            });
+        }
+    }
+}
